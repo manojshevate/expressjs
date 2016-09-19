dafc292bbc7588e49381a0697e5460551a0ef34c
Manoj Shevate <manoj.shevate@gmail.com>
2016-09-19 23:31:33 +0530

    Added static file handlers

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
+// Routing
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.post('/', function (req, res) {
+  res.send('Got a POST request');
+});
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+});
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+});
+
+
+
+// Start Web server
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
